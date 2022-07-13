0a230d59cf7cad159aecb23608d223551783a2cc
Thiago Cavallari <thiago.cavallari@scanbrazilconsulting.com>
2022-07-13 13:17:18 -0300

    ajuste baseUrl nova tentativa

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "rd34fi"
-    //baseUrl: 'http://localhost:46096/api/v1/'
+    projectId: "rd34fi",
+    baseUrl: 'http://localhost:4200/#/home'
     
   },
 });
