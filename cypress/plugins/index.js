f02cef321dacdf45dd217f7be0614c32b0f5fcc0
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-18 15:50:13 -0300

    Ajustes WorkFlow

---
+++ cypress/plugins/index.js
@@ -0,0 +1,8 @@
+export default (on, config) => {
+    on('before:browser:launch', (browser, launchOptions) => {
+      if (browser.name === 'chrome' && browser.isHeadless) {
+        launchOptions.args.push('--disable-gpu');
+        return launchOptions
+      }
+    });
+  }
