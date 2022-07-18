76b6ed973a1ed59307227a0aeac9170a9d0a9107
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-18 13:54:41 -0300

    Ajustes - inclusao de parametros script

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     projectId: "rd34fi",
+    chromeWebSecurity: false,
     baseUrl: 'http://127.0.0.1:4200/#/home'
     
   },
