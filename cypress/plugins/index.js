081e9e739484f298903495527dc4a275a2748b42
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-19 10:54:31 -0300

    Ajuste package

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,16 +1,16 @@
-export default (on, config) => {
-    on('before:browser:launch', (browser, launchOptions) => {
-      if (browser.name === 'chrome' && browser.isHeadless) {
-        launchOptions.args.push('--disable-gpu');
-        return launchOptions
-      }
-    });
-  }
-
-//   module.exports = (on, config) => {
-//     /** the rest of your plugins... **/
-//     /** the rest of your plugins... **/
-//     require('cypress-log-to-output').default.install(on)
-//     // or, if there is already a before:browser:launch handler, use .browserLaunchHandler inside of it
-//     // @see https://github.com/flotwig/cypress-log-to-output/issues/5
+// export default (on, config) => {
+//     on('before:browser:launch', (browser, launchOptions) => {
+//       if (browser.name === 'chrome' && browser.isHeadless) {
+//         launchOptions.args.push('--disable-gpu');
+//         return launchOptions
+//       }
+//     });
 //   }
+
+// //   module.exports = (on, config) => {
+// //     /** the rest of your plugins... **/
+// //     /** the rest of your plugins... **/
+// //     require('cypress-log-to-output').default.install(on)
+// //     // or, if there is already a before:browser:launch handler, use .browserLaunchHandler inside of it
+// //     // @see https://github.com/flotwig/cypress-log-to-output/issues/5
+// //   }
