6f41a69dd4ed249b82537015e0110c1529bb8124
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-20 14:20:23 -0300

    Ajuste workFlow

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -28,3 +28,8 @@ export default (on, config) => {
       on("task", percyHealthCheck);
   };
   
+module.exports = (on, config) => {
+    require('@cypress/code-coverage/task')(on, config);
+    on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
+    return config;
+  };
