c7badee7a792fcca72eee2797fabcee6b7cbacd7
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-20 11:50:19 -0300

    Ajuste WorkFlow

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,4 +19,12 @@
 export default (on, config) => {
     // `on` is used to hook into various events Cypress emits
     // `config` is the resolved Cypress config
-  }
+    on("task", {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      });
+      on("task", percyHealthCheck);
+  };
+  
