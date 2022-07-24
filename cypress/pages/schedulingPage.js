85f8580df4be891d69e1b86af4a536839fae50d3
Thiago Cavallari de Almeida <scan.tcavallari@voeazul.com.br>
2022-07-24 04:12:32 -0300

    Ajuste Worflow

--- cypress/pages/schedulingPage.js
+++ cypress/pages/schedulingPage.js
@@ -25,6 +25,7 @@ class PayrollPage {
                 const toastText = text;
                 expect(toastText).to.equal(" Agendamento cadastrado com sucesso! ");
             })
+
     }
 
     schedulingDownload() {
